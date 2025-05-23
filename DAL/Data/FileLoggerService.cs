c0b8730da496421c4829af8b117b9542b43d4e51
dharvi-kaneriya <dharvikaneriya@gmail.com>
2025-05-23 09:37:56 +0530

    feature: Converted the .NET 8 to .NET 9

---
+++ DAL/Data/FileLoggerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    public class FileLoggerService:ILoggerService
+    {
+        private readonly string logFile = "log.txt";
+
+        public FileLoggerService()
+        {
+            if (!File.Exists(logFile))
+                File.Create(logFile).Close();
+        }
+
+        public void Log(string message)
+        {
+            File.AppendAllText(logFile, $"{DateTime.Now}: {message}{Environment.NewLine}");
+        }
+    }
+}
